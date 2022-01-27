19c5a4c61ffe2506903bde151878186cdf2b2f6c
ShatlykBerdiyew <shatlykberdiev@gmail.com>
2022-01-27 22:35:08 +0500

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+function Food({name}){
+  console.log(name);
+  return (
+      <h3>I love {name}</h3>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <h1>Hello</h1>
+      <Food name="Borsh" />
+      <Food name="Palow" />
+    </div> 
   );
 }
 
